6519e7e4ae0ba96505b818e828aa443c9d2aae5f
jwhlll <jwhlll@hanmail.net>
2021-03-23 09:06:23 +0900

    resolver 파일 위치 변겅

--- src/component/product/product.module.ts
+++ src/component/product/product.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Product } from 'src/component/product/entity/product.entity';
 import { ProductService } from 'src/component/product/product.service';
-import { ProductResolver } from 'src/component/product/resolver/product.resolver';
+import { ProductResolver } from 'src/component/product/product.resolver';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Product])],
